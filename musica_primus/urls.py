ba7ba1f4368b5d22eca7227f5fe74aef99265ace
Brandon Benton <71544074+bdbenton1123@users.noreply.github.com>
2022-10-06 18:27:20 -0500

    Delete musica_primus directory

--- musica_primus/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from musica_primus import views
-
-urlpatterns = [
-    path('', views.musica_primus, name='musica_primus'),
-]
--- musica_primus/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def musica_primus(request):
-    return render(request, 'musica_primus.html', {})
